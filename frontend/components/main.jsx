09792d6f088f152b4b9cd28c24e1d7dc6b28af97
Thomas Dorrian <thomasdrrn93@gmail.com>
2017-10-07 12:27:13 -0400

    bloodhound

--- frontend/components/main.jsx
+++ frontend/components/main.jsx
@@ -5,11 +5,32 @@ class Main extends React.Component{
     super(props);
   }
 
+  componentDidMount(){
+    let results = new Bloodhound({
+      datumTokenizer: function(data) {
+        return Bloodhound.tokenizers.whitespace(data.value);
+      },
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      remote: {
+        url: 'https://api.themoviedb.org/3/search/movie?query=%QUERY&api_key=c6f1a6811bf4dfe91346ab0c38fec056',
+        filter: function(movies) {
+          return $.map(movies.results, function(movie) {
+            return {
+              value: movie.original_title,
+              id: movie.id
+            };
+          });
+        }
+      }
+    });
+    let promise = results.initialize();
+  }
+
   render(){
     return(
       <div>
         <form>
-          <input class="typeahead" type="text"/>
+          <input className="typeahead" type="text"/>
         </form>
       </div>
     );
